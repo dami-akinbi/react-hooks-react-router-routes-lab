091560a1843e0d5378b69531370374500f37d6e8
damilolaakinbi <akinbidamilola074@gmail.com>
2023-02-23 09:20:52 +0100

    NavBar.js builts, some styles added.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,32 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
+const styleLink = {
+  padding: ".75em",
+  background: "black",
+  margin: "1em",
+  color: "white",
+  borderRadius: ".5em",
+};
+
+const activeLink = { background: "#4F4F4F" };
+
 function NavBar() {
   return (
-    <>
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/movies">Movies</NavLink>
-      <NavLink to="/directors">Directors</NavLink>
-      <NavLink to="/actors">Actors</NavLink>
-    </>
+    <div style={{ marginTop: "1.5em" }}>
+      <NavLink exact to="/" style={styleLink} activeStyle={activeLink}>
+        Home
+      </NavLink>
+      <NavLink to="/movies" style={styleLink} activeStyle={activeLink}>
+        Movies
+      </NavLink>
+      <NavLink to="/directors" style={styleLink} activeStyle={activeLink}>
+        Directors
+      </NavLink>
+      <NavLink to="/actors" style={styleLink} activeStyle={activeLink}>
+        Actors
+      </NavLink>
+    </div>
   );
 }
 
