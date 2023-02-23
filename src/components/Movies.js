b00108ffb875d80153f8ba9475508cbbf256b5f3
damilolaakinbi <akinbidamilola074@gmail.com>
2023-02-23 09:27:41 +0100

    Home.js and Movies.js built

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,25 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  // console.log("Movies:", movies);
+  return (
+    <>
+      <h1>Movies Page</h1>
+      {movies.map((movie) => {
+        return (
+          <div key={movie.title}>
+            <h2>{movie.title}</h2>
+            <p>{movie.time} minutes</p>
+            <ul>
+              {movie.genres.map((genre) => (
+                <li key={genre}>{genre}</li>
+              ))}
+            </ul>
+          </div>
+        );
+      })}
+    </>
+  );
 }
 
 export default Movies;
