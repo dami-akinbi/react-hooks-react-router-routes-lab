af02689a12857cf7650a4ec6be3fef725ac88b4a
damilolaakinbi <akinbidamilola074@gmail.com>
2023-02-23 09:01:19 +0100

    App.js and NavBar.js components built

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,14 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <>
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/movies">Movies</NavLink>
+      <NavLink to="/directors">Directors</NavLink>
+      <NavLink to="/actors">Actors</NavLink>
+    </>
+  );
 }
 
 export default NavBar;
